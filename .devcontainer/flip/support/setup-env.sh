ca62a4ddc95f1cb560706bb5705fc82356a6a46e
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2025-04-14 04:31:18 -0400

    flip/docker: change folder nomenclature

--- .devcontainer/flip/support/setup-env.sh
+++ .devcontainer/flip/support/setup-env.sh
@@ -1,10 +1,4 @@
-TOOLCHAIN_ARCH=`uname -m`
-if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
-	export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-   
-	export PREFIX=/opt/aarch64-linux-gnu/aarch64-linux-gnu/libc/usr 
-else
-	export PATH="/opt/aarch64-linux-gnu/aarch64-none-linux-gnu/bin:${PATH}:/opt/aarch64-linux-gnu/aarch64-linux-gnu/libc/bin"
-	export CROSS_COMPILE=/opt/aarch64-linux-gnu/bin/aarch64-none-linux-gnu-
-	export PREFIX=/opt/aarch64-linux-gnu/aarch64-linux-gnu/libc/usr
-fi
+export PATH="/opt/aarch64-none-linux-gnu/aarch64-none-linux-gnu/bin:${PATH}:/opt/aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/usr/bin"
+export CROSS_COMPILE=/opt/aarch64-none-linux-gnu/bin/aarch64-none-linux-gnu-  
+export PREFIX=/opt/aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/usr 
 export UNION_PLATFORM=my355
--- .devcontainer/flip/support/setup-toolchain.sh
+++ .devcontainer/flip/support/setup-toolchain.sh
@@ -2,20 +2,30 @@
 
 SYSROOT_TAR="miyoo355_sdk_release.20241121"
 SYSROOT_URL=""https://github.com/steward-fu/website/releases/download/miyoo-flip/$SYSROOT_TAR.tgz""
-TOOLCHAIN_NAME="aarch64-linux-gnu"
+TOOLCHAIN_NAME="aarch64-none-linux-gnu"
+
 cd ~
-wget $SYSROOT_URL
-tar xf $SYSROOT_TAR.tgz
-rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
-rm -rf ./usr $SYSROOT_TAR.tgz
 
 TOOLCHAIN_ARCH=`uname -m`
 if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
-	echo "toolchain is aarch64, exiting"
+	echo "toolchain is aarch64"
+	TOOLCHAIN_TAR="gcc-arm-10.3-2021.07-aarch64-arm-none-linux-gnueabihf.tar.xz"
+	TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel/$TOOLCHAIN_TAR.tar.xz"
+
+	wget $TOOLCHAIN_URL
+
+	tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
+	mv -T /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
+	rm $TOOLCHAIN_TAR.tar.xz
+
+	wget $SYSROOT_URL
+	tar xf $SYSROOT_TAR.tgz
+	rsync -a --ignore-existing ./miyoo355_sdk_release/staging/usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
+	rm -rf ./usr $SYSROOT_TAR.tgz
 	exit
 fi
 
-echo "building toolchain"
+echo "toolchain is x86_64 (cross compile)"
 
 TOOLCHAIN_TAR="gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu"
 TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel/$TOOLCHAIN_TAR.tar.xz"
@@ -23,5 +33,10 @@ TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021
 wget $TOOLCHAIN_URL
 
 tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
-mv /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
+mv -T /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
 rm $TOOLCHAIN_TAR.tar.xz
+
+wget $SYSROOT_URL
+tar xf $SYSROOT_TAR.tgz
+rsync -a --ignore-existing ./miyoo355_sdk_release/staging/usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
+rm -rf ./usr $SYSROOT_TAR.tgz
