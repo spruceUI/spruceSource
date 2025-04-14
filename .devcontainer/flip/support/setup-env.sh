adde91772bf9142238f60e2e34bdfdf9ce52ce1f
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2025-04-14 00:32:20 -0400

    Add devcontainer for Miyoo Flip

---
+++ .devcontainer/flip/support/setup-env.sh
@@ -0,0 +1,10 @@
+TOOLCHAIN_ARCH=`uname -m`
+if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
+	export CROSS_COMPILE=/usr/bin/aarch64-linux-gnu-   
+	export PREFIX=/usr 
+else
+	export PATH="/opt/aarch64-linux-gnu/aarch64-linux-gnu/bin:${PATH}:/opt/aarch64-linux-gnu/aarch64-linux-gnu/libc/bin"
+	export CROSS_COMPILE=/opt/aarch64-linux-gnu/bin/aarch64-linux-gnu-
+	export PREFIX=/opt/aarch64-linux-gnu/aarch64-linux-gnu/libc/usr
+fi
+export UNION_PLATFORM=my355
