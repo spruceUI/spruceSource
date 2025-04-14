094a428425dda36f6cb8617ac62c37a5c6cd5048
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2025-04-14 12:27:12 +0530

    Add cross platform option to launch container as arm64 (#6)
    * Use Steward Fu's toolchain, add cross-platform docker option

--- .devcontainer/flip/support/setup-toolchain.sh
+++ .devcontainer/flip/support/setup-toolchain.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
 
+SYSROOT_TAR="miyoo355_sdk_release.20241121"
+SYSROOT_URL=""https://github.com/steward-fu/website/releases/download/miyoo-flip/$SYSROOT_TAR.tgz""
+TOOLCHAIN_NAME="aarch64-linux-gnu"
+cd ~
+wget $SYSROOT_URL
+tar xf $SYSROOT_TAR.tgz
+rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
+rm -rf ./usr $SYSROOT_TAR.tgz
+
 TOOLCHAIN_ARCH=`uname -m`
 if [ "$TOOLCHAIN_ARCH" = "aarch64" ]; then
 	echo "toolchain is aarch64, exiting"
@@ -8,22 +17,11 @@ fi
 
 echo "building toolchain"
 
-SYSROOT_TAR="miyoo355_sdk_release.20241121"
-TOOLCHAIN_NAME="aarch64-linux-gnu"
-TOOLCHAIN_TAR="gcc-arm-8.3-2019.02-x86_64-aarch64-linux-gnu"
-
-TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.02/$TOOLCHAIN_TAR.tar.xz"
-SYSROOT_URL=""https://github.com/steward-fu/website/releases/download/miyoo-flip/$SYSROOT_TAR.tgz""
-
-cd ~
+TOOLCHAIN_TAR="gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu"
+TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/10.3-2021.07/binrel/$TOOLCHAIN_TAR.tar.xz"
 
 wget $TOOLCHAIN_URL
-wget $SYSROOT_URL
 
 tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
 mv /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
 rm $TOOLCHAIN_TAR.tar.xz
-
-tar xf $SYSROOT_TAR.tgz
-rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
-rm -rf ./usr $SYSROOT_TAR.tgz
